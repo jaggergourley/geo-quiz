be8dd0dd0e452d4e0aab890ca183b6922224cf14
Jagger Gourley <jaggergourley@gmail.com>
2024-07-03 12:11:02 -0500

    Able to render country polygons for smaller dataset

--- script.js
+++ script.js
@@ -2,35 +2,34 @@ import * as THREE from "three";
 import ThreeGlobe from "three-globe";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-import countries from "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson";
+fetch("./custom.geojson")
+  .then((res) => res.json())
+  .then((countries) => {
+    // Setup Globe
+    const Globe = new ThreeGlobe()
+      .globeImageUrl("./8k_earth_daymap.jpg")
+      .polygonsData(countries.features)
+      .polygonStrokeColor(() => "#111");
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+    // Setup renderer
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setAnimationLoop(animate);
+    document.getElementById("globe-container").appendChild(renderer.domElement);
 
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
+    // Setup scene
+    const scene = new THREE.Scene();
+    scene.add(Globe);
+    scene.add(new THREE.AmbientLight(0xcccccc, Math.PI));
+    scene.add(new THREE.DirectionalLight(0xffffff, 0.6 * Math.PI));
 
-const Globe = new ThreeGlobe()
-  .globeImageUrl("./8k_earth_daymap.jpg")
-  .polygonsData(countries.features);
+    // Setup camera
+    const camera = new THREE.PerspectiveCamera();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    camera.position.z = 500;
 
-scene.add(Globe);
-
-const sunLight = new THREE.DirectionalLight(0xffffff);
-sunLight.position.set(-2, 0.5, 1.5);
-scene.add(sunLight);
-
-camera.position.z = 4;
-
-function animate() {
-  //  earth.rotation.x += 0.005;
-  earth.rotation.y += 0.005;
-  renderer.render(scene, camera);
-}
+    function animate() {
+      renderer.render(scene, camera);
+    }
+  });
