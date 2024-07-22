1fefacfcc444445cf281045b0945e5ac8e096129
Jagger Gourley <jaggergourley@gmail.com>
2024-07-22 14:04:30 -0500

    Working on continent selection, need to clean up functions

--- script.js
+++ script.js
@@ -3,9 +3,98 @@ import ThreeGlobe from "three-globe";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { TrackballControls } from "three/addons/controls/TrackballControls.js";
 
-$("#checkbox-all").click(function () {
-  $(this.form.elements).filter(":checkbox").prop("checked", this.checked);
+$("#checkbox-all").click(function (event) {
+  if (this.checked) {
+    $(":checkbox").each(function () {
+      this.checked = true;
+    });
+  } else {
+    $(":checkbox").each(function () {
+      this.checked = false;
+    });
+  }
 });
+
+// Append numbers in range start-end to array
+function appendNumbers(arr, start, end) {
+  for (let i = start; i <= end; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+// Shuffle array using Fisher-Yates
+function shuffle(arr) {
+  let curIndex = arr.length;
+
+  // While there are elements to shuffle
+  while (curIndex != 0) {
+    // Pick random element
+    let randIndex = Math.floor(Math.random() * curIndex);
+    curIndex--;
+
+    // Swap with current element
+    [arr[curIndex], arr[randIndex]] = [arr[randIndex], arr[curIndex]];
+  }
+
+  return arr;
+}
+
+function getCountryNumbers() {
+  let checkboxAll = document.getElementById("checkbox-all");
+  let checkboxAfrica = document.getElementById("checkbox-africa");
+  let checkboxAsia = document.getElementById("checkbox-asia");
+  let checkboxEurope = document.getElementById("checkbox-europe");
+  let checkboxNorth = document.getElementById("checkbox-north");
+  let checkboxSouth = document.getElementById("checkbox-south");
+  let checkboxOceania = document.getElementById("checkbox-oceania");
+
+  countryNums = [];
+
+  if (checkboxAll.checked) {
+    // 0-199
+    appendNumbers(countryNums, 0, 199);
+    // return, don't need to check others
+    countryNums = shuffle(countryNums);
+    return countryNums;
+  }
+  // Otherwise, check each box and add specific numbers
+  if (checkboxAfrica.checked) {
+    // 83-136
+    appendNumbers(countryNums, 83, 136);
+  }
+  if (checkboxAsia.checked) {
+    // 23-70
+    appendNumbers(countryNums, 23, 70);
+  }
+  if (checkboxEurope.checked) {
+    // 137-182
+    appendNumbers(countryNums, 137, 182);
+  }
+  if (checkboxNorth.checked) {
+    // 0-22
+    appendNumbers(countryNums, 0, 22);
+  }
+  if (checkboxSouth.checked) {
+    // 71-82
+    appendNumbers(countryNums, 71, 82);
+  }
+  if (checkboxOceania) {
+    // 183-198
+    appendNumbers(countryNums, 183, 198);
+  }
+
+  countryNums = shuffle(countryNums);
+  return countryNums;
+}
+
+// let input = document.getElementById("input-text");
+// let awesomplete = new Awesomplete(input, {
+//   minChars: 1,
+//   maxItems: 10,
+//   autoFirst: true,
+// });
+// awesomplete.list = [];
 
 fetch("./custom1.geojson")
   .then((res) => res.json())
@@ -54,15 +143,56 @@ fetch("./custom1.geojson")
       requestAnimationFrame(animate);
     })();
 
+    let countryNums = getCountryNumbers();
+    let i = -1;
+
     // Function to select and focus on random country
-    function selectRandomCountry() {
+    function selectRandomCountry(countryNums, i) {
       //console.log(countries.features.length);
-      let randIndex = Math.floor(Math.random() * countries.features.length); // number of countries in dataset
-      countries.features[randIndex].geometry.coordinates;
+      //let randIndex = Math.floor(Math.random() * countries.features.length); // number of countries in dataset
 
-      let longitude = countries.features[randIndex].properties.label_x;
-      let latitude = countries.features[randIndex].properties.label_y;
+      // want to iterate through countries 1 by one, on click
+      countries.features[countryNums[i]].geometry.coordinates;
 
+      let longitude = countries.features[countryNums[i]].properties.label_x;
+      let latitude = countries.features[countryNums[i]].properties.label_y;
+
+      let countryName = countries.features[countryNums[i]].properties.name;
+
+      console.log(countryName);
+
+      Globe.rotation.set(
+        THREE.MathUtils.degToRad(latitude),
+        THREE.MathUtils.degToRad(-longitude),
+        0,
+        "XYZ"
+      );
+
+      Globe.polygonCapColor(({ properties }) => {
+        return properties.name === countryName
+          ? "rgba(0, 200, 0, 1.0)"
+          : "rgba(0, 0, 0, 0.01)";
+      });
+      Globe.polygonAltitude(({ properties }) => {
+        return properties.name === countryName ? 0.01 : 0.001;
+      });
+
+      let area = area(countryNums[i]);
+
+      if (area >= 1000) {
+        camera.position.z = 250;
+      } else if (area >= 50) {
+        camera.position.z = 200;
+      } else if (area >= 20) {
+        camera.position.z = 175;
+      } else if (area >= 5) {
+        camera.position.z = 150;
+      } else {
+        camera.position.z = 135;
+      }
+    }
+
+    function area(index) {
       let maxLongitude = longitude;
       let minLongitude = longitude;
       let maxLatitude = latitude;
@@ -123,9 +253,6 @@ fetch("./custom1.geojson")
         }
       }
 
-      let countryName = countries.features[randIndex].properties.name;
-
-      console.log(countryName);
       console.log(countries.features[randIndex].geometry.type);
       console.log("latitude: " + latitude + " longitude: " + longitude);
       console.log(
@@ -139,48 +266,15 @@ fetch("./custom1.geojson")
           minLongitude
       );
 
-      Globe.rotation.set(
-        THREE.MathUtils.degToRad(latitude),
-        THREE.MathUtils.degToRad(-longitude),
-        0,
-        "XYZ"
-      );
-
-      Globe.polygonCapColor(({ properties }) => {
-        return properties.name === countryName
-          ? "rgba(0, 200, 0, 1.0)"
-          : "rgba(0, 0, 0, 0.01)";
-      });
-      Globe.polygonAltitude(({ properties }) => {
-        return properties.name === countryName ? 0.01 : 0.001;
-      });
-
-      // Thinking we could set zoom based on high & low lat/long to have basically a
-      // square that defines the scale of our zoom. We could find area of that
-      // square then define zoom pos as proportional to that square
-
-      // height would be abs(maxLat) - abs(minLat)
-      // width would be abs(maxLong) - abs(minLong)
-      // except when it crosses
-
       let areaHeight = maxLatitude - minLatitude;
       let areaWidth = maxLongitude - minLongitude;
       let area = areaHeight * areaWidth;
       console.log("Area: " + area);
-
-      if (area >= 1000) {
-        camera.position.z = 250;
-      } else if (area >= 50) {
-        camera.position.z = 200;
-      } else if (area >= 20) {
-        camera.position.z = 175;
-      } else if (area >= 5) {
-        camera.position.z = 150;
-      } else {
-        camera.position.z = 135;
-      }
     }
 
+    const start = document.getElementById("btn-start");
+    start.addEventListener("click");
+
     const next = document.getElementById("btn-next");
-    next.addEventListener("click", selectRandomCountry);
+    next.addEventListener("click");
   });
